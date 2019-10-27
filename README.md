e492410ee127c28c24e4c7f08a25d0211eff8eb3
Vladislav Dobrea <vlad.d@goparrot.ai>
2019-10-27 11:29:37 +0200

    Init todo example

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # lesson-20
+
+1. Implement adding a new todo item to the list 
+2. Implement remove from list functionality
+3. Implement total todo count
+4. Implement status for todo
---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+const addButton = document.getElementById("todo-add");
+const todoList = document.getElementById("todo-list");
+const todoInput = document.getElementById("todo-input");
+
+function addToDo() {
+}
+
+addButton.addEventListener('click', addToDo)
+
