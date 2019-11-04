e0553af8db6cde0b2455fb0ed288b353a0553daa
andrewstamb <andrei.stambul@gmail.com>
2019-11-04 21:46:54 +0200

    add change

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-
-
-const addButton = document.getElementById("todo-add");
-const todoList = document.getElementById("todo-list");
-const todoInput = document.getElementById("todo-input");
-
-function addToDo() {
+class Menu {
+    constructor(elem) {
+      this._elem = elem;
+      elem.onclick = this.onClick.bind(this); 
+    }
+    but1() {
+      alert('You clicked First button');
+      console.log('capturing', 'this.tagName');
+    }
+    but2() {
+      alert('You clicked Second button');
+      console.log('capturing', 'this.tagName');
+    }
+    but3() {
+      alert('You clicked Third button');
+      console.log('capturing', 'this.tagName')
+    }
+    onClick(event) {
+      let action = event.target.dataset.action;
+      if (action) {
+        this[action]();
+    }
+  };
 }
 
-addButton.addEventListener('click', addToDo)
-
+new Menu(menu);
